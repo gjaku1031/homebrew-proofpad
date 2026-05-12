a19ff0af2da0076b6c30575e4d83ee538ca1086c
gjaku1031 <gjaku1031@gmail.com>
2026-05-12 19:30:24 +0900

    feat: add proofpad cask

---
+++ Casks/proofpad.rb
@@ -0,0 +1,19 @@
+cask "proofpad" do
+  version "0.1.6"
+  sha256 "227fe32da51aa96cae30c8810465a759a1097962b28860c658090d00981228d6"
+
+  url "https://github.com/gjaku1031/ProofPad/releases/download/v#{version}/ProofPad-#{version}.dmg",
+      verified: "github.com/gjaku1031/ProofPad/"
+  name "ProofPad"
+  desc "PDF note app for marking documents with a Wacom pen"
+  homepage "https://github.com/gjaku1031/ProofPad"
+
+  auto_updates true
+
+  app "ProofPad.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.ken.proofpad.plist",
+    "~/Library/Application Support/ProofPad",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# ProofPad Homebrew Tap
+
+Personal Homebrew tap for [ProofPad](https://github.com/gjaku1031/ProofPad).
+
+```sh
+brew install --cask gjaku1031/proofpad/proofpad
+```
+
+ProofPad uses Sparkle for in-app updates. Homebrew is mainly for first install and for manual update checks:
+
+```sh
+brew update
+brew upgrade --cask --greedy proofpad
+```
