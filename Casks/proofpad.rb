85da08532e21babfd376966ec19c19cefa97aa6f
gjaku1031 <gjaku1031@gmail.com>
2026-05-12 19:32:11 +0900

    fix: satisfy cask audit

--- Casks/proofpad.rb
+++ Casks/proofpad.rb
@@ -2,8 +2,7 @@ cask "proofpad" do
   version "0.1.6"
   sha256 "227fe32da51aa96cae30c8810465a759a1097962b28860c658090d00981228d6"
 
-  url "https://github.com/gjaku1031/ProofPad/releases/download/v#{version}/ProofPad-#{version}.dmg",
-      verified: "github.com/gjaku1031/ProofPad/"
+  url "https://github.com/gjaku1031/ProofPad/releases/download/v#{version}/ProofPad-#{version}.dmg"
   name "ProofPad"
   desc "PDF note app for marking documents with a Wacom pen"
   homepage "https://github.com/gjaku1031/ProofPad"
@@ -13,7 +12,7 @@ cask "proofpad" do
   app "ProofPad.app"
 
   zap trash: [
-    "~/Library/Preferences/com.ken.proofpad.plist",
     "~/Library/Application Support/ProofPad",
+    "~/Library/Preferences/com.ken.proofpad.plist",
   ]
 end
