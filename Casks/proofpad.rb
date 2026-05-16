01c0effa93e007325110120784a6ed58f17f4fcd
gjaku1031 <gjaku1031@gmail.com>
2026-05-16 19:53:29 +0900

    chore: proofpad cask 0.1.9 업데이트

--- Casks/proofpad.rb
+++ Casks/proofpad.rb
@@ -1,6 +1,6 @@
 cask "proofpad" do
-  version "0.1.8"
-  sha256 "2ad4499baae8405eba4addd8f961757468271cf344998cfc26f9d5c5a9f972ea"
+  version "0.1.9"
+  sha256 "43455836c1261871e927bbe976f46a5b9301db88c45ff9461dac7e87431ca03c"
 
   url "https://github.com/gjaku1031/ProofPad/releases/download/v#{version}/ProofPad-#{version}.dmg"
   name "ProofPad"
